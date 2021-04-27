bf2e94a1d85a7fbb2727b57fd6e04da8442d068b
Noah Stinson <noahstjohn@ymail.com>
2021-04-27 00:01:24 -0500

    feel the sphere done

--- lesson-1/src/script.js
+++ lesson-1/src/script.js
@@ -3,34 +3,98 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Loading
+    // Load Textures
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load('/textures/golf-normal.png');
+
+
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// basically geometry/shape of something
+const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
 
 // Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// "clothing" if you will
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.normalMap = normalTexture;
+material.transparent = true;
+material.opacity = 0.5;
+material.color = new THREE.Color(0xc7079);
 
 // Mesh
+// ties body and skin together (geometry and material)
 const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+scene.add(sphere);
+
+// Objs is body, materials are skin, mesh brings the two together
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+// LIGHT 2
+ 
+// adding second light with color
+const pointLight2 = new THREE.PointLight(0xff0000, 2);
+pointLight2.position.set(-0.04, -0.93, 0.69); // x, y, z
+pointLight2.intensity = 10;
+scene.add(pointLight2);
+
+// const light1 = gui.addFolder('Light 1');
+
+// light1.add(pointLight2.position, 'x').min(-6).max(6).step(0.01);
+// light1.add(pointLight2.position, 'y').min(-3).max(3).step(0.01);
+// light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01);
+// light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01);
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 0.3);
+// scene.add(pointLightHelper);
+
+// LIGHT 3
+const pointLight3 = new THREE.PointLight(0x7cc8ed, 2);
+pointLight3.position.set(-1.45, 0.66, 0.4); // x, y, z
+pointLight3.intensity = 2.58;
+scene.add(pointLight3);
+
+// const light2 = gui.addFolder('Light 2');
+
+// light2.add(pointLight3.position, 'x').min(-6).max(6).step(0.01);
+// light2.add(pointLight3.position, 'y').min(-3).max(3).step(0.01);
+// light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01);
+// light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01);
+
+// // edit color on gui
+// const light2Color = {
+//     color: 0x7cc8ed,
+// }
+
+// light2.addColor(light2Color, 'color').onChange(() => {
+//     pointLight3.color.set(light2Color.color);
+// });
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 0.3);
+// scene.add(pointLightHelper2);
+
+
+
+
+
 
 /**
  * Sizes
@@ -72,8 +136,10 @@ scene.add(camera)
 /**
  * Renderer
  */
+// alpha: true makes threejs background transparent and 
+//      shows just website contents
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas, alpha: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -81,16 +147,48 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
+// add event listeners
+
+// changes the movement of the object when moving it
+document.addEventListener('mousemove', onDocumentMouseMove);
+
+let mouseX = 0;
+let mouseY = 0;
+let targetX = 0;
+let targetY = 0;
+
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowHalfX);
+    mouseY = (event.clientY - windowHalfY);
+}
+
+function updateSphere(event) {
+    sphere.position.y = window.scrollY * 0.002;
+}
+
+window.addEventListener('scroll', updateSphere);
+
 
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    targetX = mouseX * 0.0025;
+    targetY = mouseY * 0.0025;
+
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y = 0.6 * elapsedTime
+
+    sphere.rotation.y += 0.5 * (targetX - sphere.rotation.y);
+    sphere.rotation.x += 0.05 * (targetY - sphere.rotation.x);
+    sphere.position.z += -0.05 * (targetY - sphere.rotation.x);
+    sphere.material.opacity = 1.5 * (Math.abs(targetY));
 
     // Update Orbital Controls
     // controls.update()
